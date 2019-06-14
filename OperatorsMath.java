3bd954ba708953910e515ff0b8d44cae26d887a6
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-06-14 17:01:05 +0530

    operator in java

---
+++ OperatorsMath.java
@@ -0,0 +1,17 @@
+public class OperatorsMath 
+{
+public static void main(String [] args)
+{
+int a=10 ;
+int b=5;
+
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a%b);
+System.out.println(a/b);
+System.out.println(a*b);
+int result=a>b?a:b;
+System.out.println(result);
+System.out.println(Math.min(a,b));
+System.out.println(Math.max(a,b));
+}}
