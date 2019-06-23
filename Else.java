f1de5df8153ffc1ec1864d580309f7aab5a58051
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-06-23 14:56:44 +0530

    check a number is +ve or -ve using if statement
    @simple program of if else statement it can also be done using simple if statement

---
+++ Else.java
@@ -0,0 +1,15 @@
+public class Else
+{
+public static void main(String [] args)
+{
+int x=-34;
+if(x>0)
+{
+	System.out.println("number is positive");
+}
+else
+{
+ System.out.println("number is negative");	
+}
+}
+}
