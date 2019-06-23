59b70ee04fa4916718078f163ded7edc790eff0e
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-06-23 16:04:06 +0530

    Find even odd using while loop.

---
+++ Odd.java
@@ -0,0 +1,19 @@
+public class Odd
+{
+public static void main(String [] args)
+{
+int x=1;
+while(x<=10)
+{
+if(x%2==1)
+{
+System.out.println(x);
+}
+else
+{
+System.out.println("even"+x);	
+}
+x++;
+}
+}
+}
