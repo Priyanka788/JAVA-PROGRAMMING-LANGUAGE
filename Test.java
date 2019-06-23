aaa70c546a137a25b3bb5e4f67c622e9e34ec8bc
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-06-23 15:06:23 +0530

    hackerrank program
    #hackerrank program of weired and not weired.

---
+++ Test.java
@@ -0,0 +1,25 @@
+public class Test
+{
+public static void main(String [] args)
+{
+ int N=3;
+if(N%2==1)
+                {
+			        System.out.println(" Weird");
+			    }
+else {
+	 if(N>=2&&N<=5)
+                {
+	   		         System.out.println("Not Weird");
+				}				
+else if (N>=6&&N<=20)
+                {
+                     System.out.println(" Weird");
+                }  
+else 
+                {
+				     System.out.println("Not Weird");
+				}					 
+}
+}
+}      			
