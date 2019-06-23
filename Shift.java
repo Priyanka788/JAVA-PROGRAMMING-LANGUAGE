1726dff9b7bb3aec933ef3ba3d739172e2d69bea
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-06-23 14:49:47 +0530

    Shift operator
    simple program to explain all the shift operator in java programming

---
+++ Shift.java
@@ -0,0 +1,14 @@
+public class Shift
+{
+public static void main(String [] args)
+{
+int a=5;
+int b=5;
+int c=4;
+int y=5>>>1;
+System.out.println(a>>1);
+System.out.println(b<<1);
+System.out.println(c>>1);
+System.out.println(y);
+}
+}
