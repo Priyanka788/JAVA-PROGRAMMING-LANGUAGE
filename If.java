0335d208e479f73777d73acb221803fa894366f8
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-06-23 14:51:43 +0530

    using if else check even odd of a number
    simple if else program to check weather a number is even or odd

---
+++ If.java
@@ -0,0 +1,14 @@
+public class If
+{
+public static void main(String [] args)
+{
+int x=10;
+if(x%2==0)
+{
+System.out.println("number is even");
+}
+else{
+System.out.println("number is odd");
+}
+}
+}
