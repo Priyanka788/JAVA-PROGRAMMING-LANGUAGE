a6295e3ea1ce9c9d13d009f2deb37bcf16c0901f
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-06-23 15:24:52 +0530

    program of vowel simple if else statement
    find the given character is vowel or consonanat using if else and operator in java

---
+++ Vowel.java
@@ -0,0 +1,15 @@
+public class Vowel
+{
+public static void main(String [] args)
+{
+char ch ='b';
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+   {
+       System.out.println("Vowel"); 
+   }
+else
+   {
+	    System.out.println("Consonant"); 
+   }
+}
+}   
