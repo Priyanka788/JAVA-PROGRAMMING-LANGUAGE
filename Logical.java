d639f2f0006de69779605b8e60f4ddc708577418
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-06-23 14:47:42 +0530

    Logical operator in java
    #This is a simple logical operator program in java programming language

---
+++ Logical.java
@@ -0,0 +1,12 @@
+public class Logical
+{
+public static void main(String [] args)
+{
+int a=3;
+int b=5;
+int c=45;
+System.out.println(a>b&&b>c);
+System.out.println(a<b||b<c);
+System.out.println(!(a>b));
+System.out.println(~a);
+}}
