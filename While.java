b9ea8527488d549d40b3451deedab0fc2f347f5a
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-06-23 15:57:28 +0530

    while loop in java program

---
+++ While.java
@@ -0,0 +1,12 @@
+public class While
+{
+public static void main(String [] args)
+{
+int x=10;                     //x=1 to print 1,2,.....10.
+while(x>=1)                    //x<=10
+{
+System.out.println(x);
+x--;                      //x++
+}
+}
+}
