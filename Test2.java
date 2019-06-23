ef0827237ad54758cee8fbba9e96c3fee45871fd
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-06-23 14:59:09 +0530

    greater of four number using is else statement
    using if else and operator of java programming to find greater of four (4) number.

---
+++ Test2.java
@@ -0,0 +1,28 @@
+public class Test2
+{
+public static void main(String [] args)
+{
+ int a=2204;
+ int b=222;
+ int c=221;
+ int d =220;
+ if (a>b)
+ {
+ if(a>c&&b>c)
+ {
+   if(a>d&&d>c){
+	System.out.println("a is grater");
+	}
+	else  {
+	System.out.println("d is grater");
+	}}
+	
+	else {
+	System.out.println("c is grater");
+	}} 
+	else
+	{
+	System.out.println("b is grater");
+	
+	}}
+	}
