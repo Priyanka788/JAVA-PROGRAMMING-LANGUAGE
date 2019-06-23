5ec5cbde9141b4c6d4cb568fff4351a558b1d839
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-06-23 15:26:16 +0530

    same vowel program but with if else ladder.
    same #vowel program but using if else ladder in java programming language....................................

---
+++ Vowels.java
@@ -0,0 +1,32 @@
+public class Vowels
+{
+public static void main(String [] args)
+{
+char ch ='b';
+if(ch=='a')
+   {
+       System.out.println("Vowel"); 
+	      if(ch=='e')
+             {
+               System.out.println("Vowel");
+			    if(ch=='i')
+                  {
+                    System.out.println("Vowel");
+					  if(ch=='o')
+                        {
+                          System.out.println("Vowel");
+						    if(ch=='u')
+                               {
+                                 System.out.println("Vowel");
+								}
+							else{System.out.println("Consonant");}
+						}
+					  else{System.out.println("Consonant");}
+				   }
+				else{System.out.println("Consonant");}
+			   }
+		  else{System.out.println("Consonant");}
+     }
+else{System.out.println("Consonant");}
+}
+}	 
