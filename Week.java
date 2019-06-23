d7a296020cf527381ca9d6c372373da96018126c
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-06-23 15:02:54 +0530

    if else ladder program of days of week
    @ print days of week using if else ladder in java simplest program

---
+++ Week.java
@@ -0,0 +1,39 @@
+public class Week
+{
+public static void main(String [] args)
+{
+int  N=5;
+if (N==1)
+{
+System.out.println("Monday");
+}
+else if(N==2)
+{
+System.out.println("Tuesday");
+}
+else if(N==3)
+{
+System.out.println("Wednesday");
+}
+else if(N==4)
+{
+System.out.println("Thusday");
+}
+else if(N==5)
+{
+System.out.println("Friday");
+}
+else if(N==6)
+{
+System.out.println("Saturday");
+}
+else if(N==7)
+{
+System.out.println("Sunday");
+}
+else
+{
+System.out.println("Default Statement");
+}
+}
+}
