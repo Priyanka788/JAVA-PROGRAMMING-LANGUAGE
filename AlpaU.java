1407e58422c92a8ae171baa2752da6e50e0beab1
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 20:54:38 +0530

    Print pattern of alphabet  character "U".using java programming language..

---
+++ AlpaU.java
@@ -0,0 +1,21 @@
+public class AlpaU
+{
+public static void main(String [] args)
+
+{
+for(int row=0;row<7;row++)
+{
+   for(int col=0;col<=4;col++)
+    { 
+	    if(((col==0||col==4)&&row<6)||(row==6&&(col>0&&col<4)))
+		{
+			System.out.print("* ");
+		}
+        else
+		{
+			System.out.print("  ");
+		}		
+	}System.out.println();
+}System.out.println();
+}
+}	
