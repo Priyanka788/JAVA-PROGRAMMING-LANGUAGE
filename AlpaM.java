beca97b395d56dcf7f981ce4789c59b97163b7b3
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 20:51:34 +0530

    Print pattern of alphabet  character "M".using java programming language..

---
+++ AlpaM.java
@@ -0,0 +1,21 @@
+public class AlpaM
+{
+public static void main(String [] args)
+
+{
+for(int row=0;row<7;row++)
+{
+   for(int col=0;col<=4;col++)
+    { 
+	    if((row<=6&&(col==0||col==4))||(row==1&&(col==1||col==3))||(col==2&&(row==2||row==3)))
+		{
+			System.out.print("* ");
+		}
+        else
+		{
+			System.out.print("  ");
+		}		
+	}System.out.println();
+}System.out.println();
+}
+}	
