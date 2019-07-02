32e51043c91f415cffa012b80340b5cb9ffbbcde
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 22:59:20 +0530

    Print pattern of row in pyramid like given in description..
           1
         2    2
       3    3    3
    4    4    4    4

---
+++ Demoi.java
@@ -0,0 +1,21 @@
+public class Demoi
+{
+public static void main(String [] args)
+{
+ int x=1; 
+ for(int i=1;i<6;i++)
+{ 
+   for(int k=4;k>=i;k--)
+     {
+         System.out.print(" "); 
+     }
+   for(int j=1;j<=i;j++)
+   {
+	System.out.print(i);
+	System.out.print("  "); 
+   }
+   
+   System.out.println("\n");
+}
+}
+}
