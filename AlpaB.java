dfc57f906fd25e6990bc69258790fde629893fe6
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 20:39:31 +0530

    Print a alphabet character "B" ..using java programming language.

---
+++ AlpaB.java
@@ -0,0 +1,21 @@
+public class AlpaB
+{
+public static void main(String [] args)
+
+{
+for(int row=0;row<7;row++)
+{
+   for(int col=0;col<=4;col++)
+    { 
+	    if(((row<=6&&col==0)||((row==0||row==6||row==3)&&col<=3))||(col==4&&(row==1||row==2||row==4||row==5)))
+		{
+			System.out.print("* ");
+		}
+        else
+		{
+			System.out.print("  ");
+		}		
+	}System.out.println();
+}System.out.println();
+}
+}	
