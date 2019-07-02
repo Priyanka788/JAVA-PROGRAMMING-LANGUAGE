0569724cecb7fa18dce33b5f00ec11106c6e4032
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 21:13:20 +0530

    Continue statement in java.

---
+++ Continu.java
@@ -0,0 +1,20 @@
+public class Continu
+{
+public static void main(String [] args)
+{
+ int x=0;
+ for(x=0;x<10;x++)
+  {   
+       
+  if(x==5)
+	   {
+	   continue;
+	  
+	   }
+	       System.out.println(x);
+	   
+	   
+}  
+}
+}			
+			
