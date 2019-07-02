af6c389634182fe6f1ab59176a74c69cb890714f
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 23:35:24 +0530

    Program to Find Prime Number

---
+++ Prime.java
@@ -0,0 +1,25 @@
+public class Prime
+{
+	public static void main(String args[])
+{
+for(int i=2;i<=50;i++)
+{
+	int count=0;
+ for(int j=1;j<=i;j++)
+ { 
+   if((i%j)==0)
+     {
+	   count++;
+	 }
+  }
+ if(count==2)
+{
+  System.out.println(+i+"is a prime number");
+}
+ else
+{
+     System.out.println(+i+"is a not  prime number");
+ }
+ }
+ }
+}
