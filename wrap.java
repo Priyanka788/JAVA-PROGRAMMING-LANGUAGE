77968de3fc09daa0f7d3625a459052a9a7b33f3e
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 00:01:45 +0530

    Program to use math function like power in wrapper classes..
    Math.min
    Math.max
    Math.pow
    Math.sqrt
    and more..........//

---
+++ wrap.java
@@ -0,0 +1,19 @@
+import java.lang.Math;
+public class wrap
+{
+public static void main (String args[])
+{
+  int x=30;
+  double y=45;
+  /*System.out.println(+Math.max(x,y));
+  System.out.println(+Math.min(x,y));
+  System.out.println(+Math.pow(x,y));
+  System.out.println(+Math.sqrt(x));
+  System.out.println(+Math.log(x));
+  System.out.println(+Math.log(y));
+  System.out.println(+Math.sin(x));*/
+  System.out.println(Math.asin(x));
+  //System.out.println(+Math.cos(x));
+  //System.out.println(+Math.ceil(x));
+  }
+  }
