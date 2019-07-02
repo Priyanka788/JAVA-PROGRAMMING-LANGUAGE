4c15c3d63569ddd61fe59b1104277d46ab720c4f
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 20:40:42 +0530

    Print a alphabet character "P" ..using java programming language.

---
+++ AlpaP.java
@@ -0,0 +1,22 @@
+public class AlpaP
+{
+public static void main(String [] args)
+
+{
+for(int row=0;row<7;row++)
+{
+   for(int col=0;col<=4;col++)
+    { 
+	    if(((row<=6&&col==0)||((row==0||row==3)&&col<=3))||(col==4&&(row>0&&row<3)))
+		{
+			System.out.print("* ");
+		}
+        else
+		{
+			System.out.print("  ");
+		}		
+	}System.out.println();
+}System.out.println();
+}
+}
+	
