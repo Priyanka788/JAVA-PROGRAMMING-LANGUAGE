bc644107acb9c1543771a34fd22f4f56db51fa11
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 23:50:41 +0530

    Simple use of  command line program

---
+++ Commang.java
@@ -0,0 +1,12 @@
+public class Commang                    
+{
+public static void  main(String args[])
+{
+	//int i;
+	//for(i=args.length-1;i>=0;i--)
+	//{
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+	//}
+}
+}	
