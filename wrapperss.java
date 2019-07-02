4f1d131561807107a8a9a859f5b0a187fd2f57f1
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 23:58:53 +0530

    Program use of Integer.ParseInt method of wrapper classes,..

---
+++ wrapperss.java
@@ -0,0 +1,12 @@
+import java.lang.Double;
+public class wrapperss                            //parse xxx
+{
+public static void  main(String args[])
+{
+//int a=Integer.parseInt("1234");
+double a=Double.parseDouble("10.0");             
+//float a=Float.parseFloat("1234");
+
+System.out.println(a);
+}
+}
