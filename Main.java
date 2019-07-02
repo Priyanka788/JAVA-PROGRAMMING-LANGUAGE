874b0efb2eb0ab0f535c01d2b2c691ca2a1e6b3c
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 23:23:59 +0530

    Program to pass String from function using class

---
+++ Main.java
@@ -0,0 +1,18 @@
+ class Pjj
+{
+  String s;
+ void show(String i)
+ {
+     s=i;
+	 System.out.println(s);
+ }
+}
+public class Main
+{
+   public static void main(String args[])
+   { 
+      Pjj p=new Pjj();
+	  p.show("HelloWorld");
+	}
+}	
+	 
