fb2b3dc54bae9801ac7dcfe19f802ce4f117d0d4
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 22:49:50 +0530

    Print A to Z ,a to z and their asscii value
    Print A to Z

---
+++ Atoz.java
@@ -0,0 +1,17 @@
+public class Atoz
+{
+  public static void main(String [] args)
+{
+for(char c='a';c<='z';c++)
+{
+System.out.println(c);
+System.out.println(c+"="+(int)c);
+}
+for(char c='A';c<='Z';c++)
+{
+System.out.println(c);
+System.out.println(c+"="+(int)c);
+}
+}
+}	
+  
