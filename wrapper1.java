999b88ca6ee10c38e878b5bde1c058beb8275f3a
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 23:59:49 +0530

    Program of wrapper classes use of simple Value Of method..

---
+++ wrapper1.java
@@ -0,0 +1,16 @@
+import java.lang.Double;
+public class wrapper1                       //primitive same as string
+{
+public static void  main(String args[])
+{
+	String a="10.273";
+Integer I = Integer.valueOf(10);
+Double D =Double.valueOf(a);
+Boolean B=Boolean.valueOf(true);
+Character C=Character.valueOf('p');
+System.out.println(I);
+System.out.println(D);
+System.out.println(B);
+System.out.println(C);
+}
+}
