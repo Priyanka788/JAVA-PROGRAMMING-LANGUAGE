5eb673f171d5e66155d8dfb91c426bcb80f4577c
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 20:50:33 +0530

    Print pattern of alphabet  character "H".using java programming language..

---
+++ AlpaH.java
@@ -0,0 +1,21 @@
+public class AlpaH
+{
+public static void main(String [] args)
+
+{
+for(int row=0;row<7;row++)
+{
+   for(int col=0;col<=4;col++)
+    { 
+	    if((row<7&&(col==0||col==4))||(row==3&&(col>0&&col<4)))
+		{
+			System.out.print("* ");
+		}
+        else
+		{
+			System.out.print("  ");
+		}		
+	}System.out.println();
+}System.out.println();
+}
+}	
