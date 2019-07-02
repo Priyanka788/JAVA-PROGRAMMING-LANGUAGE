f3d7eb007299ca9fa20d7634aa2ebd1b73c3fca6
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 22:47:23 +0530

    Print Fibonacci Series  in java

---
+++ Fibo.java
@@ -0,0 +1,18 @@
+public class Fibo
+{
+	public static void main(String [] args)
+{	
+     int a= 0;
+	 int b= 1;
+	 int c= 0;
+	 int n=5;
+	//System.out.println(c);
+	 for(int i=1;i<=n;i++)
+	   {  
+	       c = a+b;
+		   a=b;
+           b=c;		   
+		//System.out.println(c);
+}System.out.println(c);
+}
+}		
