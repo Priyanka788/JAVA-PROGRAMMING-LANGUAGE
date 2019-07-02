4414e18b96dfcca3a0498b28bcee80e800bc73ac
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 23:53:21 +0530

    Program to find Prime number from given series using Command line..
     Command line.....//////

---
+++ Command4.java
@@ -0,0 +1,29 @@
+public class Command4                    
+{
+public static void  main(String args[])
+{       
+ for(int i=0;i<args.length;i++)
+ {  int d = args.length;
+   System.out.println(args.length);
+	 int count=0;
+	 int c=Integer.parseInt(args[i]);
+    for(int j=1;j<50;j++)
+	{		
+       if(c%j==0)
+        {
+         
+     count++;
+        }
+    }
+     if(count==2)
+      {
+           System.out.println(c+"the number is prime");
+      }
+	else
+	{
+           System.out.println(c+"is not the prime number");
+    }		   
+ }
+}
+}
+	 
