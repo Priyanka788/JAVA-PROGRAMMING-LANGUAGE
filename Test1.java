e652b8b366c41259bc020accb63851a6b858a88a
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 23:32:39 +0530

    program of if else if leader to find greater of 4 number

---
+++ Test1.java
@@ -0,0 +1,26 @@
+public class Test1
+{
+public static void main(String [] args)
+{
+ int a=222;
+ int b=2204;
+ int c=221;
+ int d=219;
+ if (a>b)
+ {
+ if(a>c)
+ {
+ if(a>d)
+ {
+	System.out.println("a is grater");
+	}
+	else if (d>c&&d>b){
+	System.out.println("d is grater");
+	}}
+	else if (c>b) {
+	System.out.println("c is grater");
+	}}
+	
+	else {
+	System.out.println("b is grater");
+	}}}
