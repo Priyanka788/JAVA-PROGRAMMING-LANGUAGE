c3ac2b433a5d0369dbea680d705065306cd46cbc
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 22:56:00 +0530

    Print pattern of colume like given in description  ../
    0
    01
    012
    0123

---
+++ Demo1.java
@@ -0,0 +1,16 @@
+public class Demo1
+{
+public static void main(String [] args)
+{ 
+ for(int row=0;row<5;row++)
+{
+   for(int col=0;col<row;col++)
+    {   
+	System.out.print(col);
+	}
+	
+System.out.println(" ");
+}
+}
+}	
+	
