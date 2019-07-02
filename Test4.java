60d9bb0040642cd2351d15f3e96bdabae3825ab6
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 23:34:28 +0530

    Program of static block and static function

---
+++ Test4.java
@@ -0,0 +1,19 @@
+class Test4
+{
+ static int a=m1();
+ static
+ {
+   System.out.println("inside static block");
+  }
+static int m1()
+{
+ System.out.println("from m1");
+ return 20;
+}
+ public static void main(String args[])
+{
+   System.out.println("value of a :" +a);
+   System.out.println("fronm main");
+}
+}   
+ 
