89b3a5e6676a6a9ac4c6cef54ca40d5a28da7788
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 22:53:59 +0530

    Print pattern row(number)...like given in description.
    1
    22
    333
    4444

---
+++ Demo.java
@@ -0,0 +1,16 @@
+public class Demo
+{
+public static void main(String [] args)
+{ 
+ for(int row=0;row<5;row++)
+{
+   for(int col=0;col<row;col++)
+    {   
+	System.out.print(row);
+	}
+	
+System.out.println(" ");
+}
+}
+}	
+	
