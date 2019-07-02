bca4344565acbeda74f8354a423580853ac9c2de
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 22:48:01 +0530

    Find Factorial of number

---
+++ Fact.java
@@ -0,0 +1,14 @@
+public class Fact
+{
+	public static void main(String [] args)
+{	
+  int n=5;
+  int fact=5;
+  for(int i=1;i<4;i++)
+  { 
+    n=n-1;  
+   fact= fact*n;
+   
+}System.out.println(fact);
+}
+}   
