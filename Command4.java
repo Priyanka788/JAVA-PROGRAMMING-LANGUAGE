1d542738d708f9d8fcbe10451d3f28362aab4ff8
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 23:55:42 +0530

    Update Command4.java

--- Command4.java
+++ Command4.java
@@ -4,7 +4,7 @@ public static void  main(String args[])
 {       
  for(int i=0;i<args.length;i++)
  {  int d = args.length;
-   System.out.println(args.length);
+  // System.out.println(args.length);//optional
 	 int count=0;
 	 int c=Integer.parseInt(args[i]);
     for(int j=1;j<50;j++)
