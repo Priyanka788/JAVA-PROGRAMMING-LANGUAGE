8eda6bcc3f98f779326f20310a79d35b99fa18d0
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-02 23:57:55 +0530

    Program use of xxxValue &ToString method in wrapper classes.

---
+++ wrappers.java
@@ -0,0 +1,17 @@
+public class wrappers                           //xxxvalue()
+{
+public static void  main(String args[])
+{
+//Integer i1=10;
+
+//String s=Float.toString(10.45f);
+	
+//Integer i1=Integer.valueOf("1234");
+//int c=i1.intValue();
+//String i2=String.valueOf("123");
+//String c=i2.Stringvalue();
+Integer I =new Integer(130);
+System.out.println(I.StringValue());
+//System.out.println(s);
+}
+}
