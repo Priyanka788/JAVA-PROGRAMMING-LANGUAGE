85bc9bb5ed27887af4b7b074d0307370f80f4a67
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 20:11:20 +0530

    Final variable use..

---
+++ FinalI.java
@@ -0,0 +1,17 @@
+class FinalI      //It can be also done using constructor
+{
+ final int x;
+ {
+	 x=5;
+	 System.out.println(x);
+ } 
+ /*public void fn()
+ {
+ System.out.println(x);
+ }*/
+public static void main(String args[])
+{
+  FinalI obj=new FinalI();
+  //obj.fn();
+  }
+  }
