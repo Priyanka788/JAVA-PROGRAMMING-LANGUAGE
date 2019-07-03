7e1a405162ffb753cee8807205aa41382a638dd1
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 19:36:25 +0530

    print series like 1+1/2+1/3+........soon..

---
+++ DivideN.java
@@ -0,0 +1,14 @@
+public class DivideN
+{
+public static void main(String args[])
+{
+int a=1;
+int n=20;
+System.out.print(a);
+for(int i=2;i<=n;i++)
+ {
+      System.out.print(" + ");
+     System.out.print(a+"/"+i);
+  }
+}
+} 
