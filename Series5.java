be5776dffdaf8e93cf55c4b0aef3c3db202a7a04
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 19:44:56 +0530

    Print sum of series 1+2+2+3+3+3+4+4+4+4+....soon upto n..

---
+++ Series5.java
@@ -0,0 +1,16 @@
+public class Series5
+{
+public static void main(String args[])
+{
+ int n=4;
+int s=0;
+ for(int i=1;i<n;i++)
+ {
+  for(int j=1;j<=i;j++)
+  {
+	  s=s+i;
+  }
+ }
+ System.out.println(s);
+}
+} 
