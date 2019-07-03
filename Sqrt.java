9692c33fcfad36126f45ff37a1ccb26434dc6f66
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 20:03:22 +0530

    Math.Sqrt Function use in java

---
+++ Sqrt.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+public class Sqrt
+{
+public static void main(String args[])
+{
+ //float x=1/2;
+ int a=49;
+ double result =Math.sqrt(a);
+ System.out.println(result);
+ }
+ }
