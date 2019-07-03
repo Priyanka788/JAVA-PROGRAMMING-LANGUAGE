6bfae2345929bfd662f654f0ae763e3e41b78307
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 21:15:25 +0530

    Exception in java using Default try and Default catch..

---
+++ Exception.java
@@ -0,0 +1,12 @@
+class Exception
+{
+public static void main(String args[])
+{
+ //String s=null;
+ //System.out.println("String length ="+s.length());
+ System.out.println(" Arithematic Exception ="+3/0);
+ }
+ }//Exception in thread "main" java.lang.ArithmeticException: / by zero
+        //at Exception.main(Exception.java:7)
+   //Exception in thread "main" java.lang.NullPointerException
+        //at Exception.main(Exception.java:6)
