89152323de5fc61819dbccce9a20a10cf9b8bd58
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 19:32:13 +0530

    Student1 class in package packs 2 which main in Example class..

---
+++ Student1.java
@@ -0,0 +1,24 @@
+package pack2;
+public class Student1
+{
+  int roll;
+  String name;
+  public void setroll(int r)
+    {
+       roll=r;
+	}
+   public void setname(String s)
+    {
+        name=s;
+    }
+  public int getroll()
+    {
+        return roll;
+	}
+  public String getname()
+   {
+        return name;
+   }
+}
+   
+ 
