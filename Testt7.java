6e845f73d23f627e19359b9e972d038b2e92d91c
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 21:11:28 +0530

    program to count length of String..

---
+++ Testt7.java
@@ -0,0 +1,11 @@
+import java.lang.String.*;   
+class Testt7
+{
+public static void main(String args[])
+{
+	//int temp;
+	String s=new String("Hello");
+	int c=s.length();
+	System.out.println(c);
+	}
+	}
