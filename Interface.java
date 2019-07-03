6b4661942b87d744ce9968a80608a69219771f4b
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 20:45:14 +0530

    Use of interface...

---
+++ Interface.java
@@ -0,0 +1,21 @@
+interface I1      //simple interface program 
+{
+ public void f1();
+}
+class Parent implements I1
+{
+public void f1()
+ {
+ System.out.println("java");
+ }
+}
+public class Interface
+{
+public static void main(String args[])
+{
+  Parent p=new Parent();
+  p.f1;
+}
+}
+  
+    
