e78aef652d5b284773e1697baf77cb2cdf82fdcc
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 19:41:39 +0530

    Print sum of series 1/1+1/2+1/3+1/4....soon upto n..but take double output
    If we not use double than output is only 1 with integer  data type See IN PROGRAM "Series1"

---
+++ Ser.java
@@ -0,0 +1,15 @@
+public class Ser
+{
+public static void main(String args[])
+{
+  int n=5;
+  double i;            // in this if we take int then only 1 is print
+  double s=0.0;
+  for(i=1;i<=n;i++)
+  {
+   s=s+(1/i);
+   }
+   System.out.println(s);
+}
+}
+   
