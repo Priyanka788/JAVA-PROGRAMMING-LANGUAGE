0ca27a3762b61810075e421d2f02de7179356c98
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 20:50:48 +0530

    Program of input from "user"...

---
+++ User.java
@@ -0,0 +1,13 @@
+import  java .util.*;               //Input from user....
+class User
+{
+ public static void main(String args[])
+{
+ System.out.println("Enteryour name and age");
+ Scanner sc=new Scanner(System.in);
+ String name=sc.next();
+ int age =sc.nextInt();
+ System.out.println(name+"  "+age);
+ }
+ }
+ 
