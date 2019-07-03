17655b3ad9b83efcc6ad2e0745f5cc4419843e93
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 19:26:30 +0530

    This is simple program to print output using more than one  for loop output given below.
    2
    3
    4
    5
    1<=5
    3<=6
    5<=7
    7<=8
    9<=9......like this using more than one for loop

---
+++ Forr.java
@@ -0,0 +1,21 @@
+public class Forr
+{
+  public static void main(String args[])
+  {
+	 int c=5; 
+  for(int i=2;i<=c;i=i+1)
+  {
+     System.out.println(i);
+  }
+  for(int j=1;j<=9;j=j+2)
+  {
+	  for(int r=c;r<=c;r++)
+	   { 
+	        System.out.println(+j+"<="+r);
+	   }
+      c=c+1;
+      	  
+  }
+  
+}
+}   
