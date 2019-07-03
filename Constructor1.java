0689cbec0c260748644fd080f60c8c1e53a2577f
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 20:01:34 +0530

    Simple concept of constructor..

---
+++ Constructor1.java
@@ -0,0 +1,14 @@
+class Constructor
+{
+ Constructor(int n)
+ {
+  System.out.println(n);
+  }
+ }
+public class Constructor1
+{
+public static void main(String args[])
+{
+   Constructor c1=new Constructor(10);
+}
+}   
