4ec227757153a725a2aef3c4a97eba8ddf270d8a
Priyanka788 <51821861+Priyanka788@users.noreply.github.com>
2019-07-03 19:34:20 +0530

    class Example consist main of student1 class in package packs1 and student1  class in packs2

---
+++ Example1.java
@@ -0,0 +1,14 @@
+package pack1;
+import pack2.Student1;
+public class Example1
+{
+public static void main(String args[])
+{
+  Student1 s1=new Student1();
+  s1.setroll(11);
+  s1.setname("pjj");
+  System.out.println("Roll nuumber is"+s1.getroll());
+  System.out.println("name osf student is"+s1.getname());
+  }
+  }
+  
